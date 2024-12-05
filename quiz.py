264a37bc3c140d7326c4cccb0fa36fafa7a6c9e1
Roja Shrestha <rojacresta10@gmail.com>
2024-12-05 11:48:01 +0545

    roja ignored, requirements file added and qui file added

---
+++ quiz.py
@@ -0,0 +1,36 @@
+print("Welcome to my computer quiz")
+playing = input("do you want to play? ")
+if playing.lower() != "yes":
+    quit()
+
+print("okay! let's play.... :)")
+
+score= 0
+answer = input("what does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score +=1
+else:
+    print("Sorry, you're wrong!")
+
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "ramdom access memory":
+    print("Correct!")   
+    score +=1
+
+else:
+    print("Sorry, you're wrong!")
+
+answer = input("what does GPU stand for? ")
+if answer.lower() == "graphic processing unit":
+    print("Correct!")
+    score +=1
+
+else:
+    print("Sorry, you're wrong!")
+
+
+print("You got "+ str(score)+ "questions correct!")
+
+print("You got "+ str((score/3)*100)+ "%")
